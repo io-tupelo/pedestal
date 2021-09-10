0ececce969ee5961b3ef14aacef5d5ec2fb75aef
Alan Thompson <cloojure@gmail.com>
2021-09-10 16:34:56 -0700

    misc

--- test/clj/tst/tupelo/pedestal/hello.clj
+++ test/clj/tst/tupelo/pedestal/hello.clj
@@ -35,7 +35,7 @@
 
 (def routes
   (route/expand-routes
-    #{;  vvv  wildcard doesn't work using route/expand-routes
+    #{;  vvv  wildcard doesn't work using route/expand-routes. Need to use (table/table-routes ...)
       ; (tp/table-route {:verb :get :path "/*other" :route-name :catch-all :interceptors [catch-all-intc]})
       (tp/table-route {:verb :get :path "/echo" :route-name :get-echo :interceptors [tpi/echo-interceptor]})
       (tp/table-route {:verb :get :path "/hello" :route-name :get-hello :interceptors [respond-hello-intc]})}))
@@ -50,67 +50,76 @@
   (glue base-service-map {::http/join? false}))
 
 (dotest
-  (println "tst/main - enter")
   ; capture jetty logging from System/err
-   (let [sys-err-str (with-system-err-str
-                       (tp/with-server tst-service-map ; test over http using jetty server
-                         (let [resp @(http-client/get "http://localhost:8890/hello")]
-                           (is-nonblank= (grab :body resp) "Hello, World!")
-                           (is= 200 (grab :status resp))
-                           (comment ; sample response below:
-                             {:body    "Hello, World! \n",
-                              :headers {:content-security-policy           "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;",
-                                        :content-type                      "text/plain",
-                                        :date                              "Fri, 10 Sep 2021 23:00:38 GMT",
-                                        :strict-transport-security         "max-age=31536000; includeSubdomains",
-                                        :transfer-encoding                 "chunked",
-                                        :x-content-type-options            "nosniff",
-                                        :x-download-options                "noopen",
-                                        :x-frame-options                   "DENY",
-                                        :x-permitted-cross-domain-policies "none",
-                                        :x-xss-protection                  "1; mode=block"},
-                              :opts    {:method :get, :url "http://localhost:8890/hello"},
-                              :status  200}))))]
+
+   (let [sys-err-str ""] ; (with-system-err-str ...) ; seems to not always be required
      (is= "" sys-err-str)
+
+     (tp/with-server tst-service-map ; test over http using jetty server
+       (let [resp @(http-client/get "http://localhost:8890/hello")]
+         (is-nonblank= (grab :body resp) "Hello, World!")
+         (is= 200 (grab :status resp))
+         (comment ; sample response below:
+           {:body    "Hello, World! \n",
+            :headers {:content-security-policy           "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;",
+                      :content-type                      "text/plain",
+                      :date                              "Fri, 10 Sep 2021 23:00:38 GMT",
+                      :strict-transport-security         "max-age=31536000; includeSubdomains",
+                      :transfer-encoding                 "chunked",
+                      :x-content-type-options            "nosniff",
+                      :x-download-options                "noopen",
+                      :x-frame-options                   "DENY",
+                      :x-permitted-cross-domain-policies "none",
+                      :x-xss-protection                  "1; mode=block"},
+            :opts    {:method :get, :url "http://localhost:8890/hello"},
+            :status  200})))
      ; (is (not-empty? (str/fgrep "GET /greet" sys-err-str)))
      ;    eg '[qtp1379526008-32] INFO io.pedestal.http - {:msg "GET /greet", :line 80}'
      ; sometimes blank
-     )
-  (println "tst/main - exit")
-  )
+     ))
 
 (dotest
-  (discarding-system-err
-    (tp/with-service tst-service-map ; mock testing w/o actually starting jetty
-      (let [resp (tp/service-get "/echo")
-            body (edn/read-string (grab :body resp))
-            ]
-        (is= (grab :status resp) 200)
-        (is (submatch?
-              {:request  {:body           ""
-                          :headers        {"content-length" "0" "content-type" ""}
-                          :path-info      "/echo"
-                          :path-params    []
-                          :protocol       "HTTP/1.1"
-                          :remote-addr    "127.0.0.1"
-                          :request-method :get
-                          :uri            "/echo"}
-               :response nil
-               :route    {:interceptors [{:name :echo-interceptor}]
-                          :method       :get
-                          :path         "/echo"
-                          :path-params  []
-                          :path-parts   ["echo"]
-                          :route-name   :get-echo}}
-              (tp/walk-ctx-trim body)))))))
+  ; (discarding-system-err ...) ; seems to not always be required
+  (tp/with-service tst-service-map ; mock testing w/o actually starting jetty
+    (let [resp (tp/service-get "/echo")
+          body (edn/read-string (grab :body resp))
+          ]
+      (is= (grab :status resp) 200)
+      (is (submatch?
+            {:request  {:body           ""
+                        :headers        {"content-length" "0" "content-type" ""}
+                        :path-info      "/echo"
+                        :path-params    []
+                        :protocol       "HTTP/1.1"
+                        :remote-addr    "127.0.0.1"
+                        :request-method :get
+                        :uri            "/echo"}
+             :response nil
+             :route    {:interceptors [{:name :echo-interceptor}]
+                        :method       :get
+                        :path         "/echo"
+                        :path-params  []
+                        :path-parts   ["echo"]
+                        :route-name   :get-echo}}
+            (tp/walk-ctx-trim body))))))
 
 (dotest
-  (discarding-system-err
-    (tp/with-service tst-service-map ; mock testing w/o actually starting jetty
-      (let [resp (tp/service-get "/hello?name=Fred")
-            body (grab :body resp)
-            ]
-        (is= (grab :status resp) 200)
-        (is= body "Hello, Fred!")))))
+  ; (discarding-system-err ...) ; seems to not always be required
+  (tp/with-service tst-service-map ; mock testing w/o actually starting jetty
+    (let [resp (tp/service-get "/hello?name=Fred")
+          body (grab :body resp)]
+      (is= (grab :status resp) 200)
+      (is= body "Hello, Fred!")
+      (is= resp
+        {:body    "Hello, Fred!",
+         :headers {"Content-Security-Policy"           "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;",
+                   "Content-Type"                      "text/plain",
+                   "Strict-Transport-Security"         "max-age=31536000; includeSubdomains",
+                   "X-Content-Type-Options"            "nosniff",
+                   "X-Download-Options"                "noopen",
+                   "X-Frame-Options"                   "DENY",
+                   "X-Permitted-Cross-Domain-Policies" "none",
+                   "X-XSS-Protection"                  "1; mode=block"},
+         :status  200}))))
 
 
--- test/clj/tst/tupelo/pedestal/interceptors.clj
+++ test/clj/tst/tupelo/pedestal/interceptors.clj
@@ -21,58 +21,58 @@
    ::http/join?  false}) ; true => block the starting thread (want this for supervisord in prod); false => don't block
 
 (dotest
-  (discarding-system-err
-    ; ^^^ use this to discard Pedestal info msgs like:
-    ;         [main] INFO io.pedestal.http - {:msg "POST /records", :line 80}
+  ; (discarding-system-err)
+  ; ^^^ use this to discard Pedestal info msgs like:
+  ;         [main] INFO io.pedestal.http - {:msg "POST /records", :line 80}
 
-    (tp/with-service tst-service-map ; mock testing w/o actually starting jetty
-      (let [resp (ptst/response-for (tp/service-fn) :get "/echo")
-            body (edn/read-string (grab :body resp))
-            body-trimmed  (tp/walk-ctx-trim body)
-            expected  {:async?           :tupelo.parse/edn-non-parsible
-                       :bindings         #:tupelo.parse{:edn-non-parsible :tupelo.parse/edn-non-parsible}
-                       :enter-async      [:tupelo.parse/edn-non-parsible]
-                       :request          {:async-supported?   true
-                                          :body               ""
-                                          :character-encoding "UTF-8"
-                                          :content-length     0
-                                          :content-type       ""
-                                          :context-path       ""
-                                          :headers            {"content-length" "0" "content-type" ""}
-                                          :path-info          "/echo"
-                                          :path-params        []
-                                          :protocol           "HTTP/1.1"
-                                          :query-string       nil
-                                          :remote-addr        "127.0.0.1"
-                                          :request-method     :get
-                                          :scheme             nil
-                                          :server-name        nil
-                                          :server-port        -1
-                                          :servlet            :tupelo.parse/edn-non-parsible
-                                          :servlet-request    :tupelo.parse/edn-non-parsible
-                                          :servlet-response   :tupelo.parse/edn-non-parsible
-                                          :uri                "/echo"
-                                          :url-for            :tupelo.parse/edn-non-parsible}
-                       :response         nil
-                       :route            {:interceptors [{:enter nil
-                                                          :error nil
-                                                          :leave :tupelo.parse/edn-non-parsible
-                                                          :name  :echo-interceptor}]
-                                          :method       :get
-                                          :path         "/echo"
-                                          :path-params  []
-                                          :path-parts   ["echo"]
-                                          :path-re      :tupelo.parse/edn-non-parsible
-                                          :route-name   :get-echo
-                                          :io.pedestal.http.route.prefix-tree/satisfies-constraints?
-                                                        :tupelo.parse/edn-non-parsible}
-                       :servlet          :tupelo.parse/edn-non-parsible
-                       :servlet-config   nil
-                       :servlet-request  :tupelo.parse/edn-non-parsible
-                       :servlet-response :tupelo.parse/edn-non-parsible
-                       :url-for          :tupelo.parse/edn-non-parsible}
-            ]
-        (is= (grab :status resp) 200)
-        (is= body-trimmed expected)
-        ))))
+  (tp/with-service tst-service-map ; mock testing w/o actually starting jetty
+    (let [resp         (ptst/response-for (tp/service-fn) :get "/echo")
+          body         (edn/read-string (grab :body resp))
+          body-trimmed (tp/walk-ctx-trim body)
+          expected     {:async?           :tupelo.parse/edn-non-parsible
+                        :bindings         #:tupelo.parse{:edn-non-parsible :tupelo.parse/edn-non-parsible}
+                        :enter-async      [:tupelo.parse/edn-non-parsible]
+                        :request          {:async-supported?   true
+                                           :body               ""
+                                           :character-encoding "UTF-8"
+                                           :content-length     0
+                                           :content-type       ""
+                                           :context-path       ""
+                                           :headers            {"content-length" "0" "content-type" ""}
+                                           :path-info          "/echo"
+                                           :path-params        []
+                                           :protocol           "HTTP/1.1"
+                                           :query-string       nil
+                                           :remote-addr        "127.0.0.1"
+                                           :request-method     :get
+                                           :scheme             nil
+                                           :server-name        nil
+                                           :server-port        -1
+                                           :servlet            :tupelo.parse/edn-non-parsible
+                                           :servlet-request    :tupelo.parse/edn-non-parsible
+                                           :servlet-response   :tupelo.parse/edn-non-parsible
+                                           :uri                "/echo"
+                                           :url-for            :tupelo.parse/edn-non-parsible}
+                        :response         nil
+                        :route            {:interceptors [{:enter nil
+                                                           :error nil
+                                                           :leave :tupelo.parse/edn-non-parsible
+                                                           :name  :echo-interceptor}]
+                                           :method       :get
+                                           :path         "/echo"
+                                           :path-params  []
+                                           :path-parts   ["echo"]
+                                           :path-re      :tupelo.parse/edn-non-parsible
+                                           :route-name   :get-echo
+                                           :io.pedestal.http.route.prefix-tree/satisfies-constraints?
+                                                         :tupelo.parse/edn-non-parsible}
+                        :servlet          :tupelo.parse/edn-non-parsible
+                        :servlet-config   nil
+                        :servlet-request  :tupelo.parse/edn-non-parsible
+                        :servlet-response :tupelo.parse/edn-non-parsible
+                        :url-for          :tupelo.parse/edn-non-parsible}
+          ]
+      (is= (grab :status resp) 200)
+      (is= body-trimmed expected)
+      )))
 
